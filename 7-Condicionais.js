7b6541ff0e9affb7bb4343b7399c292715d109f3
Jvnecle <161715333+Jvnecle@users.noreply.github.com>
2024-09-17 08:51:50 -0300

    Add files via upload

--- 7-Condicionais.js
+++ 7-Condicionais.js
@@ -17,22 +17,29 @@ console.log(`Trabalhando com condicionais`);
     console.log("Destinos possíveis;");
     console.log(listaDeDestinos);
 
-    if(idadeComprador >= 18){
-        console.log("Comprador maior de idade")
-        listaDeDestinos.splice(1,1);//removendo item
-    }else if(estaAcompanhada == true){
-            console.log("Comprador está acompanhado")
-            listaDeDestinos.splice(1,1);//removendo item
-        }else{
-            console.log("Não é maior de idade e não vender"); 
-        }
-        
+    //if(idadeComprador >= 18){
+    //console.log("Comprador maior de idade")
+    //listaDeDestinos.splice(1,1);//removendo item
+    //}else if(estaAcompanhada == true){
+    //console.log("Comprador está acompanhado")
+    //listaDeDestinos.splice(1,1);//removendo item
+    //}else{
+    //console.log("Não é maior de idade e não vender"); 
+    //}
+
+
+    if(idadeComprador >= 18 || estaAcompanhada == true){
+        console.log("Boa Viagem!!")
+    listaDeDestinos.splice(2,1);//removendo item
+    }else{
+     console.log("Não é maior de idade e não vender"); 
     }
+     
    
     console.log(listaDeDestinos);
     
     console.log(idadeComprador > 18);
     console.log(idadeComprador < 18);
     console.log(idadeComprador <= 18);
-    console.log(idadeComprador >= 18);
+    console.log(idadeComprador >= 18)
     console.log(idadeComprador == 18);
